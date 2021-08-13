c7bca11ce44009c3790efbc35ddf77cc54c8eb71
Gc0rp <ojasgupta4321@gmail.com>
2021-08-13 16:22:50 +1000

    Added the approved usernames and rejected usernames feature.

--- swiper/lib/main.dart
+++ swiper/lib/main.dart
@@ -2,52 +2,154 @@ import 'package:flutter/material.dart';
 import 'package:rpgnamegenerator/rpgnamegenerator.dart';
 
 class Username {
-  String name = "";
+  String userName = "";
   int status = 0;
   Username({required userName, required status});
 }
 
-class UsernameTile extends StatelessWidget {
-  final String name;
-  final int status;
+typedef UsernameChecked = Function(BuildContext context, String username);
 
-  fina
-
-  const UsernameTile({Key? key, required this.name, required this.status})
+class UsernameListItem extends StatelessWidget {
+  UsernameListItem(
+      {required this.userName,
+      required this.usernameApproved,
+      required this.usernameRejected,
+      Key? key})
       : super(key: key);
+
+  String userName;
+  UsernameChecked usernameApproved;
+  UsernameChecked usernameRejected;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(name),
-      onTap: () => {},
+      title: Text(userName),
+      onTap: () {
+        usernameApproved(context, userName);
+      },
+      onLongPress: () {
+        print('Username rejected');
+        usernameRejected(context, userName);
+      },
     );
   }
 }
 
 class UsernameList extends StatefulWidget {
-  const UsernameList({Key? key}) : super(key: key);
+  const UsernameList({required this.list, Key? key}) : super(key: key);
+
+  final List<Username> list;
 
   @override
   _UsernameListState createState() => _UsernameListState();
 }
 
 class _UsernameListState extends State<UsernameList> {
-  Widget listBuilder() {
-    final username = RpgNameGenerator.getName('human', 'male');
-    return UsernameTile(name: username, status: 0);
+  int status = 0;
+  final key = GlobalKey();
+
+  final List<String> approved = [];
+  final List<String> rejected = [];
+  // final list = <Username>{};
+
+  void _userNameApproved(BuildContext context, String username) {
+    setState(() {
+      widget.list.remove(username);
+      approved.add(username);
+    });
+  }
+
+  void _userNameRejected(BuildContext context, String username) {
+    setState(() {
+      widget.list.remove(username);
+      rejected.add(username);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (context, index) {
-          return listBuilder();
-        },
-        separatorBuilder: (context, builder) {
-          return Divider();
-        },
-        itemCount: 100);
+    final String name;
+    final int status;
+    final VoidCallback changeStatus;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Swiper'),
+        ),
+        body: Center(
+          child: ListView.builder(itemBuilder: (BuildContext _context, int i) {
+            return UsernameListItem(
+              userName: RpgNameGenerator.getName('human', 'male'),
+              usernameApproved: _userNameApproved,
+              usernameRejected: _userNameRejected,
+            );
+          }),
+        ),
+        drawer: Drawer(
+            child: NavigationItems(
+                approved: this.approved, rejected: this.rejected)));
+  }
+}
+
+class NavigationItems extends StatelessWidget {
+  final List<String> approved;
+  final List<String> rejected;
+
+  NavigationItems({required this.approved, required this.rejected, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // final userList = context.dependOnInheritedWidgetOfExactType<Username>();
+    final List<String> approved;
+    final List<String> rejected;
+
+    return ListView(
+      children: [
+        ListTile(title: Text('Home'), leading: Icon(Icons.home)),
+        ListTile(
+            title: Text('Approved'),
+            leading: Icon(Icons.check),
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute<void>(builder: (BuildContext context) {
+                print('no error');
+
+                final tiles = this.approved.map((String username) {
+                  return ListTile(title: Text(username));
+                });
+
+                return Scaffold(
+                    appBar: AppBar(title: Text('Approved Usernames')),
+                    body: ListView(children: tiles.toList()));
+                // return
+              }));
+
+              //   MaterialPageRoute<void>(builder: (BuildContext context) {
+              // return Widget();
+              // }));
+            }),
+        ListTile(
+          title: Text('Rejected'),
+          leading: Icon(Icons.close),
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+              final tiles = this.rejected.map((String username) {
+                return ListTile(title: Text(username));
+              });
+
+              return Scaffold(
+                  appBar: AppBar(title: Text('Rejected Usernames')),
+                  body: ListView(
+                    children: tiles.toList(),
+                  ));
+            }));
+          },
+        ),
+      ],
+    );
   }
 }
 
@@ -55,22 +157,5 @@ void main() {
   runApp(MaterialApp(
       title: 'Swiper',
       theme: ThemeData(primaryColor: Colors.grey),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Swiper'),
-        ),
-        body: Center(child: UsernameList()),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              ListTile(title: Text('Home'), leading: Icon(Icons.home)),
-              ListTile(
-                title: Text('Approved'),
-                leading: Icon(Icons.check),
-              ),
-              ListTile(title: Text('Rejected'), leading: Icon(Icons.close)),
-            ],
-          ),
-        ),
-      )));
+      home: UsernameList(list: [])));
 }
